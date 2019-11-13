35ee2b1019e3d8ee948a9c4e9fb34701aa9ca51f
Ethan Harvey <43649122+ethanharvey98@users.noreply.github.com>
2019-11-13 09:44:48 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var request = new XMLHttpRequest();
+
 var options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -11,6 +13,23 @@ function success(pos) {
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
+    
+  // Variables for GET request
+  api_url = "http://forecast.weather.gov/MapClick.php";
+  lat = crd.latitude;
+  lon = crd.longitude;
+  FcstType = "json"
+
+  // GET request
+  request.open('GET', api_url + "?lat=" + lat + "&lon=" + lon + "&FcstType=" + FcstType, true)
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+    console.log(data);
+    // Populate Data
+    $("#forecast").after(JSON.stringify(data));
+  }
+  request.send();
+    
 }
 
 function error(err) {
